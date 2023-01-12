d4eb1e0b10d1491310dd3ff17c3278c26346961f
Kuba Kołodziej <jmk.kolodziej@gmail.com>
2023-01-12 16:32:18 +0100

    Add hover list on search category area

--- src/components/features/ProductSearch/ProductSearch.js
+++ src/components/features/ProductSearch/ProductSearch.js
@@ -5,14 +5,31 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faListUl, faSearch, faCaretDown } from '@fortawesome/free-solid-svg-icons';
 
 import styles from './ProductSearch.module.scss';
+import { getAll } from '../../../redux/categoriesRedux';
+import { useSelector } from 'react-redux';
 
-const ProductSearch = () => (
+const ProductSearch = () => {
+
+  const categories = useSelector(getAll);
+
+  return (
   <form action='' className={styles.root}>
     <div className={styles.category}>
       <FontAwesomeIcon className={styles.icon} icon={faListUl} />
-      <select name='' id=''>
-        <option value=''>Select a category</option>
-      </select>
+        <ul>
+          <li className={styles.visibleList}>
+            Select a category
+            {categories.map(category => (
+            <ul className={styles.hiddenList}>
+              <li className={styles.visibleCategory}>{category.name}
+                <ul className={styles.hiddenCategory}>
+                  <li>{category.name} category</li>
+                </ul>
+              </li>
+            </ul>
+            ))}
+          </li>
+        </ul>
       <FontAwesomeIcon className={styles.icon} icon={faCaretDown} />
     </div>
     <div className={styles.searchField}>
@@ -22,7 +39,7 @@ const ProductSearch = () => (
       </button>
     </div>
   </form>
-);
+)};
 
 ProductSearch.propTypes = {
   children: PropTypes.node,
