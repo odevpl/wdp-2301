687c75f11750f1935208875f4220bafae7781216
Kuba Kołodziej <jmk.kolodziej@gmail.com>
2023-01-15 13:46:23 +0100

    Add common Swipeable wrapper component, used on NewFurniture.js

--- src/components/features/NewFurniture/NewFurniture.js
+++ src/components/features/NewFurniture/NewFurniture.js
@@ -1,18 +1,49 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
 import styles from './NewFurniture.module.scss';
 import ProductBox from '../../common/ProductBox/ProductBoxContainer';
+import Swipeable from '../../common/Swipeable/Swipeable';
 
 class NewFurniture extends React.Component {
   state = {
     activePage: 0,
     activeCategory: 'bed',
+    pageCount: 0,
   };
 
   handlePageChange(newPage) {
     this.setState({ activePage: newPage });
   }
+
+  handlePageCountChange(newCount) {
+    this.setState({ pageCount: newCount });
+  }
+
+  getCurrentPageCountLength = () => {
+    let length = this.props.products.filter(item => item.category === this.state.activeCategory).length/8;
+    return length;
+  };
+
+  leftAction = (e) => {
+    if(this.state.activePage>0) {
+      let page= this.state.activePage - 1;
+      this.setState({activePage: page});
+    }
+    e.preventDefault();
+  };
+
+  rightAction = (e) => {
+    let currentPageCount = this.getCurrentPageCountLength();
+    let active = this.state.activePage;
+    let activeToSet = active+1;
+
+    this.handlePageCountChange(currentPageCount);
+    if(activeToSet < currentPageCount) {
+      this.handlePageChange(activeToSet);
+    }
+    e.preventDefault();
+  };
+
 
   handleCategoryChange(newCategory) {
     this.setState({ activeCategory: newCategory });
@@ -40,41 +71,43 @@ class NewFurniture extends React.Component {
     }
 
     return (
-      <div className={styles.root}>
-        <div className='container'>
-          <div className={styles.panelBar}>
-            <div className='row no-gutters align-items-end'>
-              <div className={'col-12 col-lg-auto ' + styles.heading}>
-                <h3>New furniture</h3>
-              </div>
-              <div className={'col ' + styles.menu}>
-                <ul>
-                  {categories.map(item => (
-                    <li key={item.id}>
-                      <a
-                        className={item.id === activeCategory && styles.active}
-                        onClick={() => this.handleCategoryChange(item.id)}
-                      >
-                        {item.name}
-                      </a>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-              <div className={'col-auto ' + styles.dots}>
-                <ul>{dots}</ul>
+      <Swipeable leftAction={this.leftAction.bind()} rightAction={this.rightAction.bind(this)}>
+        <div className={styles.root}>
+          <div className='container'>
+            <div className={styles.panelBar}>
+              <div className='row no-gutters align-items-end'>
+                <div className={'col-auto ' + styles.heading}>
+                  <h3>New furniture</h3>
+                </div>
+                <div className={'col ' + styles.menu}>
+                  <ul>
+                    {categories.map(item => (
+                      <li key={item.id}>
+                        <a
+                          className={item.id === activeCategory && styles.active}
+                          onClick={() => this.handleCategoryChange(item.id)}
+                        >
+                          {item.name}
+                        </a>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+                <div className={'col-auto ' + styles.dots}>
+                  <ul>{dots}</ul>
+                </div>
               </div>
             </div>
-          </div>
-          <div className={'row ' + styles.productsBox}>
-            {categoryProducts.slice(activePage * 8, (activePage + 1) * 8).map(item => (
-              <div key={item.id} className='col-12 col-md-6 col-lg-3'>
-                <ProductBox {...item} />
-              </div>
-            ))}
+            <div className='row'>
+              {categoryProducts.slice(activePage * 8, (activePage + 1) * 8).map(item => (
+                <div key={item.id} className='col-3'>
+                  <ProductBox {...item} />
+                </div>
+              ))}
+            </div>
           </div>
         </div>
-      </div>
+      </ Swipeable>
     );
   }
 }
@@ -91,11 +124,8 @@ NewFurniture.propTypes = {
     PropTypes.shape({
       id: PropTypes.string,
       name: PropTypes.string,
-      photo: PropTypes.string,
-      category: PropTypes.string,
-      price: PropTypes.number,
-      stars: PropTypes.number,
-      promo: PropTypes.string,
+      photo: PropTypes.object,
+
       newFurniture: PropTypes.bool,
     })
   ),
