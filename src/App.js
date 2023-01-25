0fe117ba253944d7ff318f40685ea982d80655e2
Luiza0607 <110295222+Luiza0607@users.noreply.github.com>
2023-01-25 17:49:32 +0100

    Add subpage search

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import ProductsCompareContainer from './components/features/ProductsCompare/Prod
 import Blog from './components/views/Blog/Blog';
 import Login from './components/views/Login/Login';
 import Register from './components/views/Register/Register';
+import Search from './components/views/Search/Search';
 
 const App = () => (
   <Provider store={store}>
@@ -26,6 +27,7 @@ const App = () => (
           <Route exact path={'/login'} component={Login} />
           <Route exact path={'/blog'} component={Blog} />
           <Route exact path={'/register'} component={Register} />
+          <Route exact path={'/search'} component={Search} />
         </Switch>
       </MainLayout>
       <ProductsCompareContainer />
