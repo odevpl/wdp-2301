862f738a57cf0f417160c7d8946eadf4ec55be8a
Konrad Brożyna <kondzio3002@gmail.com>
2023-01-25 20:29:39 +0100

    Add name, price, isFavourite in ProductCompare

--- src/components/features/ProductsCompare/ProductsCompare.js
+++ src/components/features/ProductsCompare/ProductsCompare.js
@@ -3,31 +3,42 @@ import styles from './ProductsCompare.module.scss';
 import React from 'react';
 import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faWindowClose } from '@fortawesome/free-solid-svg-icons';
+import { faWindowClose, faHeart } from '@fortawesome/free-solid-svg-icons';
 
 const ProductsCompare = ({ products, removeProductCompare }) => {
   if (products.length === 0) return null;
   return (
     <div className={styles.root}>
       <div className='container'>
-        <div className={styles.row}>
-          <div className={styles.col_10}>
-            <div className={styles.row}>
+        <div className='row'>
+          <div className='col-10'>
+            <div className='row'>
               {products.map(product => (
-                <div key={product.id} className={styles.col_3}>
+                <div key={product.id} className='col-3'>
                   <div className={styles.image}>
                     <FontAwesomeIcon
                       onClick={() => removeProductCompare(product.id)}
                       className={styles.icon}
                       icon={faWindowClose}
                     />
+                    <h6>{product.name}</h6>
                     {product.photo}
+                  </div>
+                  <div className={'row ' + styles.content}>
+                    <div className={'col ' + styles.price}>$ {product.price}</div>
+                    <div className={'col ' + styles.favourite}>
+                      {product.isFavourite === true && (
+                        <div>
+                          <FontAwesomeIcon icon={faHeart}></FontAwesomeIcon>
+                        </div>
+                      )}
+                    </div>
                   </div>
                 </div>
               ))}
             </div>
           </div>
-          <div className={styles.col_2}>
+          <div className='col-2'>
             <Button className={styles.btn} variant={'outline'}>
               Compare
             </Button>
