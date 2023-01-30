4a7c0e2fb113ba3f5d36d5bc5373f445091eccf3
Konrad Brożyna <kondzio3002@gmail.com>
2023-01-30 23:37:22 +0100

    Fix

--- src/components/features/LatestBlog/LatestBlogContainer.js
+++ src/components/features/LatestBlog/LatestBlogContainer.js
@@ -1,9 +1,11 @@
 import { connect } from 'react-redux';
 import LatestBlog from './LatestBlog';
 import { getAll } from '../../../redux/postsRedux';
+import { getViewportWidth } from '../../../redux/widthRedux';
 
 const mapStateToProps = state => ({
   posts: getAll(state),
+  viewportWidth: getViewportWidth(state),
 });
 
 export default connect(mapStateToProps)(LatestBlog);
